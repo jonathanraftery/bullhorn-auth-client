3be8f899c2b13f3f3a691d7d7847f1d19b27c5b5
Jonathan Raftery <jonathan@jonathanraftery.com>
2018-04-16 09:26:51 -0500

    include phpunit

---
+++ tests/ClientTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace jonathanraftery\Bullhorn\REST\Authentication;
+use PHPUnit\Framework\TestCase;
+
+final class ClientTest extends TestCase
+{
+    function testRetriesOnAuthorizationCodeFailure()
+    {
+    }
+}
