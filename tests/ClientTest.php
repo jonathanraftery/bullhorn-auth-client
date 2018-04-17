ca3ce7dfe7ffa674da4bcc334aade1762996cbe8
Jonathan Raftery <jonathan@jonathanraftery.com>
2018-04-16 22:07:18 -0500

    create initial test suite

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,11 +1,66 @@
 <?php
 
-namespace jonathanraftery\Bullhorn\REST\Authentication;
 use PHPUnit\Framework\TestCase;
+use jonathanraftery\Bullhorn\REST\Authentication\Client as Client;
 
 final class ClientTest extends TestCase
 {
-    function testRetriesOnAuthorizationCodeFailure()
+    /**
+     * @dataProvider credentialsProvider
+     */
+    function testCreatesSessionForValidCredentials(
+        $clientId,
+        $clientSecret,
+        $username,
+        $password
+    ) {
+        $client = new Client(
+            $clientId,
+            $clientSecret,
+            $username,
+            $password
+        );
+        $session = $client->createSession();
+        $this->assertTrue(!empty($session->BhRestToken));
+    }
+
+    function credentialsProvider()
     {
+        $credentialsFileName = __DIR__.'/data/client-credentials.json';
+        $credentialsFile = fopen($credentialsFileName, 'r');
+        $credentialsJson = fread($credentialsFile, filesize($credentialsFileName));
+        $credentials = json_decode($credentialsJson);
+        return [
+            'testing valid credentials' => [
+                $credentials->clientId,
+                $credentials->clientSecret,
+                $credentials->username,
+                $credentials->password
+            ]
+        ];
+    }
+
+    function testThrowsExceptionOnInvalidClientId()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $badClient = new Client(
+            'testing_invalid_client_id',
+            '',
+            '',
+            ''
+        );
+        $badClient->createSession();
+    }
+
+    function testThrowsExceptionOnInvalidUsername()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $badClient = new Client(
+            '',
+            '',
+            '',
+            ''
+        );
+        $badClient->createSession();
     }
 }
