2391086a48b0085728c5fb232530e441eace01ff
Jonathan Raftery <jonathan@jonathanraftery.com>
2018-04-26 11:17:29 -0500

    fix constructor with supplied data not setting data store member

--- src/Client.php
+++ src/Client.php
@@ -20,7 +20,7 @@ class Client
     public function __construct($clientId, $clientSecret, $dataStore = null)
     { 
         $this->clientId = $clientId;
-        $this->dataStore = $dataStore ?: new JsonDataStore();
+        $this->dataStore = $dataStore ? $dataStore : new JsonDataStore();
         $this->authProvider = new OAuth2Provider([
             'clientId' => $clientId,
             'clientSecret' => $clientSecret,
